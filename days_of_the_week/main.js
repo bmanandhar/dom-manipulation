cf2038bdc9d8588c859076fa0e67da5f70c703da
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2018-11-11 18:18:19 -0800

    working on days_of_the_week

---
+++ days_of_the_week/main.js
@@ -0,0 +1,13 @@
+var listItems = document.getElementsByTagName('li');
+var itemsClass = document.getElementsByClassName('special');
+for (var i = 0; i < 2; i ++) {
+  listItems[i].textContent = listItems[i].textContent + " (special day)";
+};
+for (var i = 0; i < listItems.length; i ++) {
+  listItems[i].className = 'day';
+  listItems[i].textContent = `${i+1} - ` + listItems[i].textContent;
+  if (i >= 5) {
+    listItems[i].textContent = listItems[i].textContent + " (Holiday)";
+  }
+};
+
